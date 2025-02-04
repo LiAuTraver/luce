9acbd2dc8358b9bb51603de6784dded2d1b030ea
LiAuTraver <me@liautraver.dev>
2025-02-04 20:05:54 +0800

    splited deps

--- conanfile.py
+++ conanfile.py
@@ -5,18 +5,18 @@ class LuceProject(conan.ConanFile):
     """
     Conan package definition for the Luce project. Bazel ONLY.
     >>> conan install . --output-folder=conan_installed
-    >>> bazel --bazelrc=conan_installed/conan_bzk.rc build //tools/luce:luce # seems the rc was optional
+    >>> bazel --bazelrc=conan_installed/conan_bzl.rc build //tools/luce:luce # seems the rc was optional
     """
 
     name = "luce"
     version = "1.0.0"
 
-    url = "https://conan.io"
+    url = "not.defined.yet"
     license = "Apache-2.0"
     author = "LiAuTraver"
     description = "A riscv64 emulator written in C++"
     topics = ("riscv64", "emulator", "cpp")
-    homepage = "https://conan.io"
+    homepage = "not.defined.yet"
 
     settings = "os", "compiler", "build_type", "arch"
     generators = "BazelDeps", "BazelToolchain"
--- toolchain/utils.bzl
+++
@@ -1,5 +0,0 @@
-"""
-Utility functions for Bazel build files.
-"""
-def semicolon_separated(list):
-    return ";".join(list)
