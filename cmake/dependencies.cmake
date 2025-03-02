23046203560c5f7f4b6bdfd6a04a48328b3375eb
LiAuTraver <me@liautraver.dev>
2025-03-03 02:25:04 +0800

    update dependencies to include auxilia subdirectory and remove unused packages

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -3,16 +3,11 @@ include_guard()
 find_package(fmt CONFIG REQUIRED)
 find_package(spdlog CONFIG REQUIRED)
 find_package(argparse CONFIG REQUIRED)
-# find_package(capstone CONFIG REQUIRED)
-# find_package(scn CONFIG REQUIRED)
-# find_package(magic_enum CONFIG REQUIRED)
 find_package(auxilia QUIET)
-# find_package(zstd CONFIG REQUIRED)
-# find_package(ZLIB #[[CONFIG]] REQUIRED) # zlib does not provide a config file
 
 if(NOT auxilia_FOUND)
   message(AUTHOR_WARNING "auxilia not found, using local version")
-  include_directories("${LUCE_PROJECT_ROOT_DIR}/source/auxilia/include")
+  add_subdirectory(${LUCE_PROJECT_ROOT_DIR}/source/auxilia)
 endif()
 
 add_library(external_deps INTERFACE)
@@ -20,13 +15,7 @@ target_link_libraries(external_deps INTERFACE
   fmt::fmt
   spdlog::spdlog
   argparse::argparse
-  # capstone::capstone
-  # scn::scn
-  # magic_enum::magic_enum
-  # ZLIB::ZLIB
-  # zstd::libzstd
-
-  # $<$<BOOL:${auxilia_FOUND}>:auxilia::auxilia>
+  auxilia::auxilia
 )
 target_compile_features(external_deps INTERFACE cxx_std_23)
 
